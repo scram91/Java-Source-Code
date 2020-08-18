7685c1e8a08283a47071eecf4313bb81aa33c64d
scram91 <67920223+scram91@users.noreply.github.com>
2020-08-18 11:59:09 -0500

    Update TempConv.java

--- TempConv.java
+++ TempConv.java
@@ -5,7 +5,7 @@
 
 import java.util.Scanner;
 
-public class TemperatureConversion {
+public class TempConv {
     public static void main(String args[]) {
       
       Scanner input = new Scanner(System.in);
