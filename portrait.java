50418f9f64d1a44cb200c2cb1eba1e49a86600f0
scram91 <67920223+scram91@users.noreply.github.com>
2020-08-18 11:50:44 -0500

    Create portrait.java

---
+++ portrait.java
@@ -0,0 +1,32 @@
+// portrait.java
+
+// Prints a self portrait and name in a box.
+
+
+public class portrait {
+
+   
+	public static void main(String[] args) {
+
+       
+      System.out.println(" /////// ");
+
+	     System.out.println(" | o o | ");
+
+	     System.out.println("{|  ^  |}");
+ 
+	     System.out.println(" \\\\ - //  ");
+	     System.out.println("   |||   \n\n"); //Newline character to separate portrait from box
+
+
+
+
+	     System.out.println("+----------+");
+  
+	     System.out.println("|  Samuel  |");
+     
+	     System.out.println("+----------+");
+    
+	} //end main method
+
+} //end public class portrait
